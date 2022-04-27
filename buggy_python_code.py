dec45ef2b0969d2ac633350bebcf2e5f2de27557
Vladimir Dubecky <xdubecky@fi.muni.cz>
2022-04-27 12:41:35 +0200

    some changes..

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,9 +1,10 @@
 # contains bunch of buggy examples
-# taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
-import cPickle
+# taken from 
+# https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
+
 import subprocess
 import base64
-import subprocess
+import cPickle
 import flask
 
 # Input injection
@@ -13,7 +14,7 @@ def transcode_file(request, filename):
 
 
 # Assert statements
-def foo(request, user):
+def foo(user):
     assert user.is_admin, 'user does not have access'
     # secure code...
 
@@ -23,13 +24,13 @@ class RunBinSh(object):
     def __reduce__(self):
         return (subprocess.Popen, (('/bin/sh',),))
 
-def import_urlib_version(version):
-    exec("import urllib%s as urllib" % version)
+    def import_urlib_version(version):
+        exec("import urllib%s as urllib" % version)
 
-@app.route('/')
-def index():
-    module = flask.request.args.get("module")
-    import_urlib_version(module)
+    @app.route('/')
+    def index():
+        module = flask.request.args.get("module")
+        import_urlib_version(module)
 
 
 print(base64.b64encode(pickle.dumps(RunBinSh())))
